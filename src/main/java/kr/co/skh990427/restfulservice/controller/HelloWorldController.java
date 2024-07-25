f9453bc023464bcb96d862390219dbe35928dc20
skh990427 <skh990427@naver.com>
2024-07-25 17:40:52 +0900

    path variable test

--- src/main/java/kr/co/skh990427/restfulservice/controller/HelloWorldController.java
+++ src/main/java/kr/co/skh990427/restfulservice/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package kr.co.skh990427.restfulservice.controller;
 
 import kr.co.skh990427.restfulservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable(name = "name") String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 
