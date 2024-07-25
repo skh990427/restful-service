e742531c72244152d508a42166ee10bdcb75e45c
skh990427 <skh990427@naver.com>
2024-07-25 16:35:18 +0900

    first commit

---
+++ src/main/java/kr/co/skh990427/restfulservice/RestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.skh990427.restfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/skh990427/restfulservice/RestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.skh990427.restfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
