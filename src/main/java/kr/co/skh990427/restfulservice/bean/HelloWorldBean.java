d29fe638fdcaa59a08e6afffdafbb7259cc36a76
skh990427 <skh990427@naver.com>
2024-07-25 17:23:13 +0900

    test controller

---
+++ src/main/java/kr/co/skh990427/restfulservice/bean/HelloWorldBean.java
@@ -0,0 +1,11 @@
+package kr.co.skh990427.restfulservice.bean;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Data
+public class HelloWorldBean {
+
+    private final String message;
+}
---
+++ src/main/java/kr/co/skh990427/restfulservice/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package kr.co.skh990427.restfulservice.controller;
+
+import kr.co.skh990427.restfulservice.bean.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+
+}
