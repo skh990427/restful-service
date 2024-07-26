2046a11fb2523b7300d3940438b5c13f09463a18
skh990427 <skh990427@naver.com>
2024-07-26 16:38:55 +0900

    다국어 처리 Internationalization 구현

--- src/main/java/kr/co/skh990427/restfulservice/RestfulServiceApplication.java
+++ src/main/java/kr/co/skh990427/restfulservice/RestfulServiceApplication.java
@@ -2,12 +2,24 @@ package kr.co.skh990427.restfulservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulServiceApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
 	}
 
 }
--- src/main/java/kr/co/skh990427/restfulservice/controller/HelloWorldController.java
+++ src/main/java/kr/co/skh990427/restfulservice/controller/HelloWorldController.java
@@ -1,12 +1,20 @@
 package kr.co.skh990427.restfulservice.controller;
 
 import kr.co.skh990427.restfulservice.bean.HelloWorldBean;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
+@RequiredArgsConstructor
 public class HelloWorldController {
+
+    private final MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -23,5 +31,10 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
+    @GetMapping("/hello-world-internationalize")
+    public String helloWorldInternationalize(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
 
+        return messageSource.getMessage("greeting.message", null, locale);
+    }
 }
