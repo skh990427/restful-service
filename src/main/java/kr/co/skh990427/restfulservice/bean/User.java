4796d1b39478c84ca07e052b96cee56901632185
skh990427 <skh990427@naver.com>
2024-07-26 10:04:49 +0900

    user 도메인 클래스 생성

---
+++ src/main/java/kr/co/skh990427/restfulservice/bean/User.java
@@ -0,0 +1,14 @@
+package kr.co.skh990427.restfulservice.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+    private Date joinData;
+}
---
+++ src/main/java/kr/co/skh990427/restfulservice/dao/UserDaoService.java
@@ -0,0 +1,50 @@
+package kr.co.skh990427.restfulservice.dao;
+
+import kr.co.skh990427.restfulservice.bean.User;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    private static int userCount = 3;
+
+    static {
+        users.add(new User(1, "Kenneth", new Date()));
+        users.add(new User(2, "Alice", new Date()));
+        users.add(new User(3, "Elena", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++userCount);
+        }
+
+        if (user.getJoinData() == null) {
+            user.setJoinData(new Date());
+        }
+
+        users.add(user);
+
+        return user;
+    }
+
+    public User findOne(int id) {
+        for (User user : users) {
+            if(user.getId() == id) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+}
