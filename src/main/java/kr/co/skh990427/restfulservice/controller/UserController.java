d83f6a147ac677608627b24614d0bfb71fb5af58
skh990427 <skh990427@naver.com>
2024-07-26 11:29:09 +0900

    http status code 제어

--- src/main/java/kr/co/skh990427/restfulservice/controller/UserController.java
+++ src/main/java/kr/co/skh990427/restfulservice/controller/UserController.java
@@ -3,8 +3,11 @@ package kr.co.skh990427.restfulservice.controller;
 import kr.co.skh990427.restfulservice.bean.User;
 import kr.co.skh990427.restfulservice.dao.UserDaoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,7 +26,14 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = userDaoService.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
